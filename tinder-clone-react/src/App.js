14b4f3578ee4fdda6fd849d9b08ce9b6df05bb98
Travis Bowen <Travisbowen23@gmail.com>
2020-10-09 11:23:21 -0700

    Added header component and styling

--- tinder-clone-react/src/App.js
+++ tinder-clone-react/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>App</h1>
+		<div className='app'>
+			<Header />
 		</div>
 	);
 }
